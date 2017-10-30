c2fe1e442006ce2078b33f050e6435d79c773cbd
Jacob Krogman <jacobkrogman92@gmail.com>
2017-10-30 13:10:03 -0400

    Add wrapper class and test

---
+++ wrapper.js
@@ -0,0 +1,45 @@
+function Wrapper() { }
+
+/*
+  Private array of punctuation to ignore. When the current column boundry would
+  result in punctuation being wrapped, the punctuation in this list would be
+  ignored and the next character would be the column breakpoint.
+*/
+
+var _punctuation_to_ignore = [
+  ",",
+  ".",
+  ";"
+]
+
+Wrapper.prototype.wrap = function(str, colLen) {
+    var tempStr = str, colIdx = colLen;
+    var si = 0;
+    while(colIdx < tempStr.length) {
+      var idx = colIdx;
+      var inWordBoundry = false;
+      while (tempStr[idx] !== ' ' && idx > 0) {
+        idx--;
+      }
+
+      if(colIdx - idx >= colLen){
+        idx = colIdx;
+        inWordBoundry = true;
+      }
+
+      if(inWordBoundry) {
+        if(_punctuation_to_ignore.indexOf(tempStr[idx]) > -1) {
+          tempStr = tempStr.substring(0, idx+1) + "\n" + tempStr.substring(idx+1, tempStr.length);
+        } else {
+          tempStr = tempStr.substring(0, idx) + "-\n" + tempStr.substring(idx, tempStr.length);
+        }
+      } else {
+        tempStr = tempStr.substring(0, idx) + "\n" + tempStr.substring(idx+1, tempStr.length);
+      }
+      colIdx = idx + colLen;
+    }
+    return tempStr;
+};
+
+// Export Singleton instance of wrapper;
+module.exports = new Wrapper();
---
+++ wrapper.test.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+var Wrapper = require('./wrapper.js');
+
+var testTxt = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent egestas eros felis, at gravida tortor pharetra a. Pellentesque molestie dui vitae arcu imperdiet, a dignissim libero consectetur. Pellentesque lectus neque, iaculis eget urna vel, placerat convallis nibh. Mauris placerat mi diam, sed ultrices quam fermentum a. Morbi ipsum nunc, accumsan et augue sagittis, sodales hendrerit sapien. Aenean egestas libero sapien. Sed ultrices velit turpis, id egestas dui accumsan et. Vivamus ut nisl molestie nunc ultricies ornare eu et turpis. Aenean lacus magna, scelerisque eget commodo ut, semper id nisi. Quisque feugiat porta tortor a efficitur. Aenean vel vehicula massa. Cras tellus nunc, dignissim in diam vel, euismod scelerisque nulla. Ut dui felis, dignissim ut lorem at, scelerisque laoreet lectus. Morbi tempus dui ac elit ullamcorper, sit amet pharetra orci scelerisque."
+var colLen = 120;
+
+for(var x = 1; x < 11; x++) {
+  var colLen = Math.ceil(140 / x);
+  console.log("===== RUN " + x + " colLen " + colLen + " =====")
+  var wrappedTxt = Wrapper.wrap(testTxt, colLen);
+  var arr = wrappedTxt.split("\n");
+  var result = true;
+
+  for(var i = 0; i < arr.length; i++) {
+      if(arr[i].length > colLen) result = false;
+  }
+
+  assert( result === true, 'All lines are less than column width.');
+  console.log(wrappedTxt);
+}
